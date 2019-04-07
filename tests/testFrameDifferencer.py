2b2a2465b11414c7b5aab0c5d782af8fcccb779d
Cary Symes <s3550167@student.rmit.edu.au>
2019-04-07 14:55:28 +1000

    Test data, plus data loading test cases

---
+++ tests/testFrameDifferencer.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#testFrameDifferencer.py
+"""
+Created 2019-04-04
+Series of tests for the frame subtraction functionality
+@author: Cary
+"""
+
+from quickDDM.readVideo import readVideo
+from quickDDM.frameDifferencer import frameDifferencer
+import unittest
+import numpy
+
+class FrameDifferenceTestCases(unittest.TestCase):
+    def testSubtractingIdenticalFrames(self):
+        frames = readVideo('tests/black.avi')
+        diff = frameDifferencer(frames, [1]) # subtract every set of frames with a distance of 1
+        # Should be 4 frames of 0's, as all `frames` are identical
+
+        gold = [[[0 for i in range(1024)] for j in range(1024)] for k in range(5 - 1)]
+
+        self.assertTrue(numpy.array_equal(diff[0], gold)) # Check first subtraction (first two frames) is zero
+
+    @unittest.skip("Fails - only calculates the first frame diff per spacing for now")
+    def testSubtractingUniqueFrames(self):
+        # consists of a frame of white followed by a 70% grey frame
+        frames = readVideo('tests/alternating.avi')
+        diff = frameDifferencer(frames, [1])
+
+        gold = [[[int(255*0.3) for i in range(1024)] for j in range(1024)] for k in range(5 - 1)]
+
+        self.assertTrue(numpy.array_equal(diff[0], gold)) # compare first subtraction
+
+    def testCorrectNumberOfSpacingsCalculated(self):
+        frames = readVideo('tests/black.avi')
+        diff = frameDifferencer(frames, [1, 2, 3, 4, 5])
+
+        self.assertEqual([4, 3, 2, 1, 0], [len(d) for d in diff])
+
+    def testOverlyWideSpacingFails(self):
+        frames = readVideo('tests/black.avi')
+        diff = frameDifferencer(frames, [10])
+
+        # Behaviour unspecified for the moment - can either return an empty set of subtractions
+        # or maybe throw an error? TODO - need to decide whether the function should prevent this
+        # itself or expect the calling function to prevent it
+        self.assertEqual(len(diff), 1)
+        self.assertEqual(len(diff[0]), 0)
+
+    def testSingleFrameVideoGetsNoSoup(self):
+        frames = readVideo('tests/short.avi')
+        diff = frameDifferencer(frames, [1])
+        
+        self.assertEqual(len(diff), 1)
+        self.assertEqual(len(diff[0]), 0)
---
+++ tests/testReadVideo.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#testReadVideo.py
+"""
+Created 2019-04-04
+Series of tests for the video reading/frame splitting functionality
+@author: Cary
+"""
+
+import unittest
+import cv2
+import numpy
+import contextlib, io
+from quickDDM.readVideo import readVideo
+
+class ReadVideoTestCases(unittest.TestCase):
+    def testValidVideoOpens(self):
+        # TODO maybe split readVideo into readVideo(IntoFrames) and read(Single)Frame?
+        readVideo('tests/short.avi') # single black frame
+
+    def testReadFramesAreCorrect(self):
+        frames = readVideo('tests/black.avi') # 5x frames of straight black
+        self.assertEqual(len(frames), 5)
+
+        gold = [[[0 for i in range(1024)] for j in range(1024)] for k in range(5)]
+        self.assertTrue(numpy.array_equal(frames, gold))
+
+    def testCorruptedVideoFails(self):
+        # Unfortunately this invalid read creates a semi-unblockable write to stderr
+        # Workaround: https://eli.thegreenplace.net/2015/redirecting-all-kinds-of-stdout-in-python/
+        # Just take a lot of extra code, probably not worth it
+        with self.assertRaises(cv2.error):
+            print('\nExpecting error... ', end='')
+            readVideo('tests/corrupt.avi')
+            # corrupt.avi is literally just 1MB of /dev/random
+
+    @unittest.skip("Unsure how to mock this for now")
+    def testBadPermissionsReadFails(self):
+        self.fail("Unable to read")
+
+    def testEmptyVideoFails(self):
+        with self.assertRaises(cv2.error):
+            print('\nExpecting error... ', end='')
+            frames = readVideo('tests/empty.avi')
