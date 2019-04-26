89cc5fa038bcd69a8b69e463091bf4daf4fb6491
Cary Symes <s3550167@student.rmit.edu.au>
2019-04-26 16:35:57 +1000

    Basic layout of analysis panel

--- quickDDM/ui_tk.py
+++ quickDDM/ui_tk.py
@@ -16,26 +16,37 @@ from calculateCorrelation import calculateCorrelation
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
-from tkinter.filedialog import askopenfilename
-from PIL import ImageTk, Image
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+from PIL import Image
 from PIL.ImageTk import PhotoImage
 
 import cv2
 
+from concurrent.futures import ThreadPoolExecutor
 
-ADDR_PLACEHOLDER = 'Choose a file...' # Placeholder text for filepath entry box
+
 WINDOW_PADDING = 10 # pixels
+# LoadFrame constants
+ADDR_PLACEHOLDER = 'Choose a file...' # Placeholder text for filepath entry box
 SCRUB_STEPS = 10 # previewable frames
 PREVIEW_DIM = 256 # size of the preview image (pixels, square)
+# ProcessingFrame constants
+SAMPLE_DIM = 512
 
+pool = ThreadPoolExecutor(max_workers=10)
 
 
 
 class LoadFrame(Frame):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, padding=WINDOW_PADDING)
         # prevent AttributeError when checking if it needs clearing before anything's been cleared
         self.video_file = None
+
+        self.populateL() # video load/data elements
+        self.columnconfigure(3, minsize=WINDOW_PADDING) # Gap between left and right sections
+        self.populateR() # video preview elements
 
 
 
@@ -54,9 +65,6 @@ class LoadFrame(Frame):
 
         self.loadVideo(filename)
 
-    """Handle direct file path entry"""
-    def triggerFromEntry(self, widget):
-        self.loadVideo(widget.get())
 
     """Select a video after being called by one of the above trigger methods"""
     def loadVideo(self, filename):
@@ -97,12 +105,13 @@ class LoadFrame(Frame):
 
         # File didn't open - not a video, corrupt, read error, or whatever else it could be
         else:
+            self.clearPreviews() # filepath no longer valid - clear any previous preview data
+
             messagebox.showerror('File Error',
                                  'There was an issue with loading or accessing the ' +
                                  'specified video file. Double check it\'s a video and that ' +
                                  'you have the necessary permissions')
             videoFile.release() # Release anyway
-            self.clearPreviews() # filepath no longer valid - clear any previous preview data
 
     """Delete all metadata and image preview data"""
     def clearPreviews(self):
@@ -118,11 +127,21 @@ class LoadFrame(Frame):
             self.video_file.release() # release the previous video stream
         self.video_file = None # and allow garbage collection
 
-    """Click handler for analysis button"""
+    """
+    Click handler for analysis button
+    Basically throws away the current window and creates a new Frame for the processing UI
+    """
     def triggerAnalysis(self):
+        win = self._nametowidget(self.winfo_parent())
+        filename = self.address.get()
+
         self.video_file.release()
-        # TODO create ProcessingFrame
         self.destroy()
+        # The old has passed away...
+
+        # ...behold, the new has come!
+        pframe = ProcessingFrame(win, filename)
+        pframe.grid()
 
     """Rips the frame at `index` out of the chosen video and shows in the preview pane"""
     def setPreview(self, index):
@@ -184,20 +203,20 @@ class LoadFrame(Frame):
 
         # file address label
         lAdd = Entry(self, textvariable=self.address)
-        lAdd.grid(row=1, column=1, columnspan=2, sticky=[E, W], padx=(0, WINDOW_PADDING))
+        lAdd.grid(row=0, column=0, columnspan=2, sticky=[E, W], padx=(0, WINDOW_PADDING))
         # Juggling to clear/restore placeholder text
-        lAdd.bind('<FocusIn>', lambda e: e.widget.delete(0, 'end') if 
-                                    (e.widget.get() == ADDR_PLACEHOLDER) else None)
-        lAdd.bind('<FocusOut>', lambda e: (e.widget.insert(0, ADDR_PLACEHOLDER) or self.clearPreviews()) 
-                  if (e.widget.get() == '') else self.triggerFromEntry(e.widget))
+        lAdd.bind('<FocusIn>', lambda e: self.address.set('') if 
+                                    (self.address.get() == ADDR_PLACEHOLDER) else None)
+        lAdd.bind('<FocusOut>', lambda e: (self.address.set(ADDR_PLACEHOLDER) or self.clearPreviews()) 
+                  if (self.address.get() == '') else self.loadVideo(self.address.get()))
 
         # file chooser button
         lChoose = Button(self, text='Choose Source', command=self.triggerVideoSelect)
-        lChoose.grid(row=1, column=3)
+        lChoose.grid(row=0, column=2)
 
         # metadata pane - contains all metadata labels added in the below block
         lMetadata = Frame(self, borderwidth=1, relief='solid')
-        lMetadata.grid(row = 2, column=1, 
+        lMetadata.grid(row = 1, column=0, 
                        rowspan=2, columnspan=3,
                        sticky=[E, W, N, S],
                        pady=5)
@@ -228,53 +247,157 @@ class LoadFrame(Frame):
 
         # Button to progress to next stage
         lLoad = Button(self, text='Analyse Video', command=self.triggerAnalysis)
-        lLoad.grid(row=5, column=2)
+        lLoad.grid(row=4, column=1, pady=(WINDOW_PADDING, 0))
         lLoad['state'] = 'disabled'
         self.load_button = lLoad # store button for reactivation later
 
         # Favour whitespace in favour of stretching the metadata pane
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(4, weight=10)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=10)
         # Equal width for space on both sides of the analysis button
-        self.columnconfigure(1, minsize=lChoose.winfo_reqwidth())
+        self.columnconfigure(0, minsize=lChoose.winfo_reqwidth())
 
     """Add UI elements for the video preview"""
     def populateR(self):
         # use a Canvas to draw the actual image
         preview = Canvas(self, relief='solid', borderwidth=1)
-        preview.grid(row=1, column=5, rowspan=4, sticky=N)
+        preview.grid(row=0, column=4, rowspan=4, sticky=N)
         preview.configure(width=PREVIEW_DIM, height=PREVIEW_DIM)
         self.img_preview = preview # store the canvas so we can draw onto it later
 
         # Horizontal scrollbar to allow scrubbing through the video
         # This is kinda superfluous since we're looking at such small changes anyway 🙃
         scrub = Scrollbar(self, orient='horizontal', command=self.handleScroll)
-        scrub.grid(row=5, column=5, sticky=[E, W])
+        scrub.grid(row=4, column=4, sticky=[E, W])
         self.scrub = scrub
 
-    """Add padding around the root window"""
-    def configureFrame(self):
-        self.rowconfigure(0, minsize=WINDOW_PADDING) # Padding off top of the window
-        self.rowconfigure(5, pad=WINDOW_PADDING*2) # Bump processing button/slider off window edge+canvas
-        self.columnconfigure(0, minsize=WINDOW_PADDING) # Padding off left side of the window
-        self.columnconfigure(4, minsize=WINDOW_PADDING) # Gap between left and right sections
-        self.columnconfigure(6, minsize=WINDOW_PADDING) # Padding off right side of the window
+
+
+
+class ProcessingFrame(Frame):
+    def __init__(self, parent, fname):
+        super().__init__(parent, padding=WINDOW_PADDING)
+        
+        self.populate()
+        center(parent) # Update window dimensions/placement
+
+        self.beginAnalysis(fname) # Begin processing the video
+
+
+
+    ### Processing
+
+
+    """Begin the video analysis. See basicMain.py"""
+    def beginAnalysis(self, fname):
+        pass # TODO everything
+
+    """Saves all current data to disk in a CSV (via a file selector)"""
+    def saveAllData(self):
+        filename = asksaveasfilename(initialdir = '.', 
+                                     title = 'Select save location', 
+                                     filetypes = (('Comma Seperated Values', '*.csv'),))
+        if not filename:
+            return # file selector aborted
+
+        # Split IO onto its own thread
+        def save(self, fn): # TODO actually save
+            import time
+            time.sleep(2)
+            print('Saved to '+ fn)
+
+        pool.submit(save, self, filename)
+        # TODO some sort of progress indicator in place of button?
+        # https://stackoverflow.com/questions/3819354/in-tkinter-is-there-any-way-to-make-a-widget-not-visible
+
+    """
+    Generates curve fitting for the current data
+    Options for `model` are 
+        'motility'
+        'brownian'
+    """
+    def curveFit(self, model):
+        print(f'Fitting with assumption/{model}')
+        # TODO
+
+
+
+    ### UI Configuration
+
+
+    """Create UI elements for viewing/analysing the processed data"""
+    def populate(self):
+        fProgress = Frame(self)#, relief='solid', borderwidth=1)
+        fProgress.grid(row=0, column=0, sticky=[E, W])
+        if fProgress:
+            # Could put the label on top of the progressbar, but it has a non-transparent background
+            # https://stackoverflow.com/questions/17039481/how-to-create-transparent-widgets-using-tkinter
+
+            self.stage = StringVar()
+            self.stage.set('test')
+            l = Label(fProgress, textvariable=self.stage)
+            l.grid(row=0, column=0)
+
+            self.progress = IntVar()
+            self.progress.set(65)
+            pBar = Progressbar(fProgress, mode='determinate', variable=self.progress)
+            pBar.grid(row=1, column=0, sticky=[E, W])
+
+            # Yeah look I don't know why I need this, but otherwise there's a bunch of wasted space
+            fProgress.columnconfigure(0, weight=10000)
+
+
+        fFitting = Frame(self)
+        fFitting.grid(row=0, column=2, sticky=E)
+        if fFitting:
+            lFitting = Label(fFitting, text='Curve fitting, assuming: ')
+            lFitting.grid(row=0, column=0)
+
+            bBrownian = Button(fFitting, text='Brownian Motion', command=lambda: self.curveFit('brownian'))
+            bDirectional = Button(fFitting, text='Directional Motion', command=lambda: self.curveFit('motility'))
+            bSave = Button(fFitting, text='Save All to Disk...', command=self.saveAllData)
+
+            bBrownian.grid(row=0, column=1)
+            bDirectional.grid(row=0, column=2, padx=(WINDOW_PADDING, WINDOW_PADDING*4))
+            bSave.grid(row=0, column=3)
+
+        pResults = Listbox(self)
+        pResults.grid(row=2, column=0, sticky=[E, W, N, S])
+
+        pCurves = Canvas(self, relief='solid', borderwidth=1)
+        pCurves.configure(width=SAMPLE_DIM, height=SAMPLE_DIM)
+        pCurves.grid(row=2, column=2)
+
+        self.rowconfigure(1, minsize=WINDOW_PADDING) # buttons and list/canvas
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, minsize=WINDOW_PADDING) # Gap between left and right sections
+        self.columnconfigure(2, weight=3)
+        # TODO allow resizing?
+
+
 
 """Set the window's position"""
 def center(win):
-    # Current window dimensions
-    w = win.winfo_height()
-    h = win.winfo_width()
-
     # Screen width/height
     ww = win.winfo_screenwidth()
     wh = win.winfo_screenheight()
 
-    # Calculate window coordinates
+
+    win.geometry(f'+{ww*2}+{wh*2}') # move offscreen before rendering
+    win.update() # generate geometry before moving window
+
+
+    # Current window dimensions
+    w = win.winfo_width()
+    h = win.winfo_height()
+
+    # and move back onscreen at correct position
     x = ((ww//2) - (w//2))
     y = ((wh//2) - (h//2))
 
-    win.geometry(f'{h}x{w}+{x}+{y}') # And set them
+
+    win.geometry(f'+{x}+{y}') # And set them
+    # win.geometry(f'{w}x{h}+{x}+{y}') # W/H auto-calculated - don't need to force them
 
 if __name__ == '__main__':
     # Create a new Tk framework instance / window
@@ -283,13 +406,10 @@ if __name__ == '__main__':
     loader = LoadFrame(window)
     loader.grid()
 
-    loader.populateL() # video load/data elements
-    loader.populateR() # video preview elements
-    loader.configureFrame()  # window margins, etc.
 
-    window.update() # generate geometry before moving window
     center(window) # set window location
     window.winfo_toplevel().title("quickDDM")
     window.resizable(False, False) # It's not really super responsive
 
-    window.mainloop() # hand control off to tkinter
+    with pool: # Ensure thread pool gets shutdown
+        window.mainloop() # hand control off to tkinter
