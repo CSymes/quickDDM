da78f65077ffee51cf8d7a9bf3101e58df9beee7
s3604656 <s3604656@student.rmit.edu.au>
2019-05-07 12:26:24 +1000

    Patched to add fftshift
    This is a tiny change, but will need to be reflected in all other variants from other branches when they're merged in.

--- quickDDM/twoDFourier.py
+++ quickDDM/twoDFourier.py
@@ -4,6 +4,7 @@
 Created on Wed Mar 27 12:28:43 2019
 Takes in a list of 3d arrays of frames, and runs the 2d fourier transform on
 each frame
+As of hotfix01, all variants should be updated to have an fftshift
 @author: Lionel
 """
 
@@ -16,7 +17,7 @@ def twoDFourier(framesArray):
     #TODO: it is possible we can halve the size of the complex data by using rfft2
     #link: https://stackoverflow.com/questions/52387673/what-is-the-difference-between-numpy-fft-fft-and-numpy-fft-rfft/52388007
     #will need to test it thouroughly though
-    framesArray = np.fft.fft2(framesArray)
+    framesArray = np.fft.fftshift(np.fft.fft2(framesArray))
     return normaliseFourier(framesArray)
 
 def normaliseFourier(frames):
