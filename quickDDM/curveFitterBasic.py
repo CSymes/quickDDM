d95f90e0cb784de291775d92ca93a1cc35d1ed17
s3604656 <s3604656@student.rmit.edu.au>
2019-05-16 17:37:35 +1000

    Added intial parameters, q in fitting

--- quickDDM/curveFitterBasic.py
+++ quickDDM/curveFitterBasic.py
@@ -14,10 +14,11 @@ import matplotlib.pyplot as plt
 """
 These functions are the various functions to which to fit the provided data
 They must take parameters:
-(torVector, *fitParams)
+(torVector, *fitParams, *, q)
 where torVector is the sequence of time differences and fitParams are the 
-parameters to adjust to fit the function. If additional fixed parameters are 
-required, create a lambda using the function with an appropriate signature
+parameters to adjust to fit the function. The *,q marks q as a named argument,
+which must be supplied. This must be present whether or not q is part of the
+function, to ensure consistency.
 They must return a single floating point value.
 """
 
@@ -26,29 +27,73 @@ They must return a single floating point value.
 #Differential Dynamic Microscopy of Bacterial Motility
 #Wilson et al
 #Published 5-JAN-2011
-#This one is useless with experiment01.avi
-def diffusionFunction(tor, q, D):
+#Note that this uses q as a parameter, meaning a lambda must be used
+def diffusionFunction(tor, D, *, q):
     return np.exp(-D*(pow(q,2))*tor)
 
 #Useable as an ultra-simple test case.
-def linearFunction(tor, m, c):
+def linearFunction(tor, m, c, *, q):
     return tor*m + c
     
 #Mimicing what is used in "Characterizing Concentraded, Multiply Scattering..."
 #This one matches the provided experiment01 file quite well
 #It is an exponential curve that decays from B up toward 2A+B
-def risingExponential(deltaT, A, B, otherTor):
-    g = np.exp(-deltaT/otherTor)
+def risingExponential(deltaT, A, B, D, *, q):
+    g = np.exp(-deltaT/D)
+    return 2*A*(1-g)+B
+   
+def risingExponentialWithQ(deltaT, A, B, D, *, q):
+    g = np.exp(-deltaT * D * q^2)
     return 2*A*(1-g)+B
    
 """
 This is the dictionary from which to access fitting functions. Modify this here
 whenever a new weighting scheme is added. DO NOT modify this during execution.
 """
-fittingFunctions ={
+FITTING_FUNCTIONS ={
     "diffusion":diffusionFunction,
     "rising exponential":risingExponential,
-    "linear":linearFunction
+    "linear":linearFunction,
+    "rising with q":risingExponentialWithQ
+}
+   
+   
+"""
+These functions serve to generate initial estimates and bounds for the fitting
+functions, to get a nice, accurate fit. Largely based on the provided MATLAB.
+The odd return signature is to best interface with the curve fitting function
+Each one has a signature:
+correlation: one correlation curve at fixed q for which to produce intial 
+estimates and bounds.
+RETURN: tuple (estimatesArray, (lowerBoundsArray, upperBoundsArray))
+"""
+
+#These are taken from the provided MATLAB
+def risingExponentialGuess(correlation):
+    Astart = np.max(correlation) - np.min(correlation)
+    Bstart = np.min(correlation)
+    Dstart = 0.3
+    Amin = Astart / 2
+    Amax = Astart * 2
+    Bmin = Bstart / 2
+    Bmax = Bstart * 2
+    Dmin = 0.01
+    Dmax = 80
+    estimates = np.array([Astart, Bstart, Dstart])
+    lowerBounds = np.array([Amin, Bmin, Dmin])
+    upperBounds = np.array([Amax, Bmax, Dmax])
+    return (estimates,(lowerBounds, upperBounds))
+    
+"""
+This is the dictionary from which to access intial guess functions. Modify this
+here whenever a new weighting scheme is added. DO NOT modify this during
+execution.
+"""
+GUESS_FUNCTIONS ={
+    "diffusion":None,#To do if required
+    "rising exponential":risingExponentialGuess,
+    "rising with q":risingExponentialGuess,
+    "linear":None
 }
    
 """
@@ -123,6 +168,7 @@ correlations: The 2d matrix of intensity(dT,q)
 qValues: list of q values at which to fit
 fitting: string specifying which function to use
 weighting: tuple(String,float) how to bias the sampling (log, linear, etc)
+zoom: parameter used to determine the size of a pixel. Typically 0.7, 1.42, 4.1
 RETURN: tuple of (list(popt),list(qIndex), function) where popt is
 the tuple of fitting parameters for a given q value, qIndex is the index within
 the correlations list that maps to that particular result's q value, and 
@@ -130,24 +176,36 @@ function is the function it fitted to.
 Values are populated to the indicies matching the Q values, all others are None
 The exception being the q indicies themselves
 #TODO: named tuples
+#TODO: this is currently doing to q correction itself; consider where that should be done
 """
-def fitCorrelationsToFunction(correlations, qValues, fitting, weighting = ("linear", (1,))):
+def fitCorrelationsToFunction(correlations, qValues, fitting, weighting = ("linear", (1,)), zoom = 0.71):
     paramResults = [None] * correlations.shape[1]
-    #TODO: correct to use real dimensions
-    #TODO: make a new tool for estimating initial values
-    if fitting == "rising exponential":
-        guess = (np.max(correlations) - np.min(correlations), np.min(correlations), 0.3)
-    else :
-        guess = None
+    #TODO: correct to use real dimensions, distribute this correction throughout
     
     scheme = WEIGHTING_SCHEMES[weighting[0]]
+    guessGenerator = GUESS_FUNCTIONS[fitting]
+    fittingFunction = FITTING_FUNCTIONS[fitting]
+    #size of one frame in pixels, calculated from maximum q
+    frameSize = (correlations.shape[1]-1)*2
+    #converts pixel q to q in um^-1, for use in fitting
+    qCorrection = (2*np.pi*zoom/(frameSize))
     for q in qValues:
         torVector = scheme(correlations[:,q], weighting[1])
+        #correcting tor to use micro seconds
         torCurve = correlations[torVector,q]
-        popt, pcov = scipy.optimize.curve_fit(fittingFunctions[fitting], torVector, torCurve, p0 = guess)
+        realQ = q * qCorrection
+        #Used to fix q on each iteration, so that it can be a part of the
+        #fittign function
+        fitLambda = lambda tor, *args: fittingFunction(tor, *args, q=realQ)
+        
+        if guessGenerator is not None:
+            (initials, paramBounds) = guessGenerator(correlations[:,q])
+        else:
+            (initials, paramBounds) = (None, None)
+        popt, pcov = scipy.optimize.curve_fit(fitLambda, torVector, torCurve, p0 = initials, bounds = paramBounds)
         paramResults[q] = popt
     
-    return (paramResults, qValues, fittingFunctions[fitting])
+    return (paramResults, qValues * qCorrection, fittingFunction, qCorrection)
 
 def plotCurveComparisonsLinear(correlations, fittingResult, qIndicies):
     fitParams = fittingResult[0]
@@ -159,7 +217,7 @@ def plotCurveComparisonsLinear(correlations, fittingResult, qIndicies):
         #Drops the first element, as it is useless and not used in fitting
         torCurve = correlations[1:,q]
         torVector = np.arange(1,len(torCurve)+1)
-        fitCurve = fitFunction(torVector, *fitParams[q])
+        fitCurve = fitFunction(torVector, *fitParams[q], q = q)
         plt.plot(torVector, torCurve, '-', color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
         plt.plot(torVector, fitCurve, '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
     plt.ylabel('\u0394(\u03B4t)')
@@ -184,7 +242,7 @@ def plotCurveComparisonsLog(correlations, fittingResult, qIndicies):
         #Have to use scatter for the data points, as it looks quite badly 
         #distorted using a line at low delta T
         plt.scatter(linTorVector, torCurve, color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
-        plt.plot(logTorVector, fitFunction(logTorVector, *fitParams[q]), '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
+        plt.plot(logTorVector, fitFunction(logTorVector, *fitParams[q], q = q), '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
     plt.xscale('symlog')
     plt.ylabel('\u0394(\u03B4t)')
     plt.xlabel('\u03B4t (Frames)')
@@ -204,12 +262,12 @@ def generateFittedCurves(fittingResult, startTor, endTor, stepTor):
     for q in qValues:
         if fitParams[q] is None:
             continue
-        fitCurve = fitFunction(torVector, *fitParams[q])
+        fitCurve = fitFunction(torVector, *fitParams[q], q=q)
         curves.append(fitCurve)
     return curves
     
 #Use this if you just want to run it in the console quickly, from a file.
 def bootstrap(path, qValues, fitting, weighting = ("linear",(1,))):
     loadedCorrelations = np.loadtxt(path)
-    fittingResult = fitCorrelationsToFunction(loadedCorrelations, qValues, fitting, weighting)
+    fittingResult = fitCorrelationsToFunction(loadedCorrelations, qValues, fitting, weighting = weighting)
     return (fittingResult, loadedCorrelations)
