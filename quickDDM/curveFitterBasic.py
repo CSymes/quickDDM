316d8d6c1388135c86c9b8561582b3ee5caaf338
s3604656 <s3604656@student.rmit.edu.au>
2019-05-22 12:04:04 +1000

    Fixed to match MATLAB
    Now produces same results as matlab code. Now handles being provided time spacings instead of assuming full density. Refactored signatures to pass required real parameters (zoom, frame rate)

--- quickDDM/curveFitterBasic.py
+++ quickDDM/curveFitterBasic.py
@@ -14,8 +14,8 @@ import matplotlib.pyplot as plt
 """
 These functions are the various functions to which to fit the provided data
 They must take parameters:
-(torVector, *fitParams, *, q)
-where torVector is the sequence of time differences and fitParams are the 
+(tauVector, *fitParams, *, q)
+where tauVector is the sequence of time differences and fitParams are the 
 parameters to adjust to fit the function. The *,q marks q as a named argument,
 which must be supplied. This must be present whether or not q is part of the
 function, to ensure consistency.
@@ -28,23 +28,21 @@ They must return a single floating point value.
 #Wilson et al
 #Published 5-JAN-2011
 #Note that this uses q as a parameter, meaning a lambda must be used
-def diffusionFunction(tor, D, *, q):
-    return np.exp(-D*(pow(q,2))*tor)
+def diffusionFunction(tau, D, *, q):
+    return np.exp(-D*(pow(q,2))*tau)
 
 #Useable as an ultra-simple test case.
-def linearFunction(tor, m, c, *, q):
-    return tor*m + c
+def linearFunction(tau, m, c, *, q):
+    return tau*m + c
     
 #Mimicing what is used in "Characterizing Concentraded, Multiply Scattering..."
 #This one matches the provided experiment01 file quite well
 #It is an exponential curve that decays from B up toward 2A+B
-def risingExponential(deltaT, A, B, D, *, q):
-    g = np.exp(-deltaT/D)
-    return 2*A*(1-g)+B
+    
    
-def risingExponentialWithQ(deltaT, A, B, D, *, q):
-    g = np.exp(-deltaT * D * q^2)
-    return 2*A*(1-g)+B
+def risingExponential(deltaT, A, B, D, *, q):
+    g = np.exp(-deltaT * D * (q**2))
+    return A*(1-g)+B
    
 """
 This is the dictionary from which to access fitting functions. Modify this here
@@ -53,8 +51,7 @@ whenever a new weighting scheme is added. DO NOT modify this during execution.
 FITTING_FUNCTIONS ={
     "diffusion":diffusionFunction,
     "rising exponential":risingExponential,
-    "linear":linearFunction,
-    "rising with q":risingExponentialWithQ
+    "linear":linearFunction
 }
    
    
@@ -92,12 +89,11 @@ execution.
 GUESS_FUNCTIONS ={
     "diffusion":None,#To do if required
     "rising exponential":risingExponentialGuess,
-    "rising with q":risingExponentialGuess,
     "linear":None
 }
    
 """
-These functions are used to create tor vectors to downsample the provided
+These functions are used to create tau vectors to downsample the provided
 correlation curve. They take parameters of 
 (correlationCurve, sampleParams)
 where correlationCurve is a single vector from the correlation function and 
@@ -126,25 +122,25 @@ Not yet implemented:
 
 def linearSpacing(correlationCurve, sampleParams):
     spacing = sampleParams[0]
-    return np.arange(1,len(correlationCurve), spacing).astype(int)
+    return np.arange(0,len(correlationCurve), spacing).astype(int)
 
 def expSpacing(correlationCurve, sampleParams):
     perDecade = sampleParams[0]
     #Because we always want the first ten values
-    oneToNine = np.arange(1,10)
+    zeroToNine = np.arange(0,10)
     #exponential spacings from 10 to the end
-    expTorVector = pow(10, np.arange(1,np.log10(len(correlationCurve)),1/perDecade))
+    expTauVector = pow(10, np.arange(1,np.log10(len(correlationCurve)),1/perDecade))
     #Cast to integer so it can be used to index
-    expTorVector = np.concatenate((oneToNine, expTorVector)).astype(int)
-    return expTorVector
+    expTauVector = np.concatenate((oneToNine, expTauVector)).astype(int)
+    return expTauVector
    
 def percentileSpacing(correlationCurve, sampleParams):
     percent = sampleParams[0]
     medianIntensity = np.percentile(correlationCurve, percent)
     exceedingElements = np.where(correlationCurve > medianIntensity)
     lastIndex = exceedingElements[0][0]
-    torVector = np.arange(1,lastIndex).astype(int)
-    return torVector
+    tauVector = np.arange(0,lastIndex).astype(int)
+    return tauVector
     
 """
 This is the dictionary from which to access weighting schemes. Modify this here
@@ -176,57 +172,73 @@ function is the function it fitted to.
 Values are populated to the indicies matching the Q values, all others are None
 The exception being the q indicies themselves
 #TODO: named tuples
-#TODO: this is currently doing to q correction itself; consider where that should be done
+#TODO: figure out if there are any off-by-one errors in indexing correlations
+#TODO: remove weighting scheme system, probably. It is more trouble than it's
+#worth, because it means we'd have to include a time spacing vector at each q
 """
-def fitCorrelationsToFunction(correlations, qValues, fitting, weighting = ("linear", (1,)), zoom = 0.71):
+def fitCorrelationsToFunction(correlations, qValues, fitting, *, weighting = ("linear", (1,)), qCorrection = 1, timeSpacings = None, frameRate = 100):
     paramResults = [None] * correlations.shape[1]
-    #TODO: correct to use real dimensions, distribute this correction throughout
     
     scheme = WEIGHTING_SCHEMES[weighting[0]]
     guessGenerator = GUESS_FUNCTIONS[fitting]
     fittingFunction = FITTING_FUNCTIONS[fitting]
     #size of one frame in pixels, calculated from maximum q
-    frameSize = (correlations.shape[1]-1)*2
+    frameSize = correlations.shape[1]*2
     #converts pixel q to q in um^-1, for use in fitting
-    qCorrection = (2*np.pi*zoom/(frameSize))
     for q in qValues:
-        torVector = scheme(correlations[:,q], weighting[1])
-        #correcting tor to use micro seconds
-        torCurve = correlations[torVector,q]
+        #Choosing which samples to take, defaults to all
+        tauVector = scheme(correlations[:,q], weighting[1])
+        tauCurve = correlations[tauVector,q]
+        if timeSpacings is None:
+            #TODO: this is going to have an off-by-one error at the moment, needs to be taken care of
+            tauVector = tauVector/frameRate
+        else : #if it has been provided
+            tauVector = timeSpacings
+        #moving from pixels to um^-1
         realQ = q * qCorrection
         #Used to fix q on each iteration, so that it can be a part of the
-        #fittign function
-        fitLambda = lambda tor, *args: fittingFunction(tor, *args, q=realQ)
-        
+        #fitting function without curve_fit trying to adjust it
+        fitLambda = lambda tau, *args: fittingFunction(tau, *args, q=realQ)
+        #Getting initial values for fitting, if they can be generated
         if guessGenerator is not None:
             (initials, paramBounds) = guessGenerator(correlations[:,q])
-        else:
+        else:#If they can't be, it uses no bounds and the default initials
             (initials, paramBounds) = (None, None)
-        popt, pcov = scipy.optimize.curve_fit(fitLambda, torVector, torCurve, p0 = initials, bounds = paramBounds)
+        popt, pcov = scipy.optimize.curve_fit(fitLambda, tauVector, tauCurve, p0 = initials, bounds = paramBounds)
         paramResults[q] = popt
     
-    return (paramResults, qValues * qCorrection, fittingFunction, qCorrection)
+    return (paramResults, np.array(qValues) * qCorrection, fittingFunction)
 
-def plotCurveComparisonsLinear(correlations, fittingResult, qIndicies):
+def plotCurveComparisonsLinear(correlations, fittingResult, qIndicies, *, qCorrection = 1, timeSpacings = None, frameRate = 100):
     fitParams = fittingResult[0]
     fitFunction = fittingResult[2]
     for i in range(0,len(qIndicies)):
         q = qIndicies[i]
+        realQ = q * qCorrection
         if fitParams[q] is None:
             continue
-        #Drops the first element, as it is useless and not used in fitting
-        torCurve = correlations[1:,q]
-        torVector = np.arange(1,len(torCurve)+1)
-        fitCurve = fitFunction(torVector, *fitParams[q], q = q)
-        plt.plot(torVector, torCurve, '-', color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
-        plt.plot(torVector, fitCurve, '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
+        
+        tauCurve = correlations[:,q]
+        
+        if timeSpacings is None:
+            tauVector = np.arange(1,len(tauCurve)+1) / frameRate
+            fitTauVector = np.arange(1,len(tauCurve)+1) / frameRate
+        else:
+            tauVector = timeSpacings
+            #In the same range, but linearly spaced
+            fitTauVector = np.arange(1, len(tauCurve) + 1) / len(timeSpacings) * np.max(timeSpacings)
+        fitCurve = fitFunction(fitTauVector, *fitParams[q], q = realQ)
+        
+        plt.plot(tauVector, tauCurve, '-', color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
+        plt.plot(fitTauVector, fitCurve, '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
     plt.ylabel('\u0394(\u03B4t)')
     plt.xlabel('\u03B4t (Frames)')
     plt.legend()
     plt.title("Linearly Scaled Correlations")
     plt.show()
     
-def plotCurveComparisonsLog(correlations, fittingResult, qIndicies):
+
+def plotCurveComparisonsLog(correlations, fittingResult, qIndicies, *, qCorrection = 1, timeSpacings = None, frameRate = 100):
     fitParams = fittingResult[0]
     fitFunction = fittingResult[2]
     
@@ -234,15 +246,21 @@ def plotCurveComparisonsLog(correlations, fittingResult, qIndicies):
         q = qIndicies[i]
         if fitParams[q] is None:
             continue
-        #Drops the first element, as it is useless and not used in fitting
-        torCurve = correlations[1:,q]
-        linTorVector = np.arange(1,len(torCurve)+1)
-        #Generates a log-spaced vector with the same number of elements, for neat plotting
-        logTorVector = np.logspace(0, np.log10(len(torCurve)), num = len(torCurve))
+        realQ = q * qCorrection
+        tauCurve = correlations[:,q]
+        linTauVector = np.arange(1,len(tauCurve)+1)
+        #Matches the spacings of the provided data, for plotting the given curve
+        if timeSpacings is None:
+            dataTauVector = linTauVector / frameRate
+        else:
+            dataTauVector = timeSpacings
+        
+        #Makes a log-spaced vector with the same range, for plotting fit
+        logTauVector = np.logspace(np.log10(np.min(dataTauVector)), np.log10(np.max(dataTauVector)), num = len(tauCurve))
         #Have to use scatter for the data points, as it looks quite badly 
         #distorted using a line at low delta T
-        plt.scatter(linTorVector, torCurve, color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
-        plt.plot(logTorVector, fitFunction(logTorVector, *fitParams[q], q = q), '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
+        plt.scatter(dataTauVector, tauCurve, color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
+        plt.plot(logTauVector, fitFunction(logTauVector, *fitParams[q], q = realQ), '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve at q=%d inverse pixels' % q)
     plt.xscale('symlog')
     plt.ylabel('\u0394(\u03B4t)')
     plt.xlabel('\u03B4t (Frames)')
@@ -253,21 +271,67 @@ def plotCurveComparisonsLog(correlations, fittingResult, qIndicies):
     plt.title("Logarithmically Scaled Correlations")
     plt.show()
 
-def generateFittedCurves(fittingResult, startTor, endTor, stepTor):
+"""
+Returns a list of numpy arrays representing the fitted points generated by the
+provided fittingResult tuple at the given time spacings or frame rate + number
+Defaults to linear spacing from 10ms to 10s.
+Ignores the frame rate and number of frames if given a time spacings vector
+"""
+def generateFittedCurves(fittingResult, qIndicies, *, timeSpacings = None, frameRate = 100, numFrames = 1000, qCorrection = 1):
     fitParams = fittingResult[0]
-    qValues = fittingResult[1]
     fitFunction = fittingResult[2]
-    torVector = np.arange(startTor, endTor + stepTor, stepTor)
+    if timeSpacings is None:
+            tauVector = np.arange(1/frameRate, (numFrames + 1) / frameRate, 1/frameRate)
+    else:
+        tauVector = timeSpacings
+    
     curves = []
-    for q in qValues:
+    for q in qIndicies:
         if fitParams[q] is None:
             continue
-        fitCurve = fitFunction(torVector, *fitParams[q], q=q)
+        realQ = q * qCorrection
+        fitCurve = fitFunction(tauVector, *fitParams[q], q=realQ)
         curves.append(fitCurve)
-    return curves
+    return np.array(curves)
+    
+
+"""
+This is primarily just test code, but you might find a use for it. For each q,
+it plots the data and the curves generated by both given fittings using the
+fitting function for fitA. fitB is only used for its fit params, other entries
+may be zero. Results are ploted by matplotlib
+"""
+def compareFittings(fitA, fitB, correlations, qIndicies, *, qCorrection = 1, timeSpacings = None, frameRate = 100):
+    fitParamsA = fitA[0]
+    fitParamsB = fitB[0]
+    fitFunction = fitA[2]
+    for i in range(0,len(qIndicies)):
+        q = qIndicies[i]
+        realQ = q * qCorrection
+        tauCurve = correlations[:,q]
+        if timeSpacings is None:
+            tauVector = np.arange(1,len(tauCurve)+1) / frameRate
+        else:
+            tauVector = timeSpacings
+        fitCurveA = fitFunction(tauVector, *fitParamsA[q], q = realQ)
+        fitCurveB = fitFunction(tauVector, *fitParamsB[q], q = realQ)
+        plt.plot(tauVector,  tauCurve,  '-', color = plt.cm.viridis(i/len(qIndicies)), label = 'Actual data at q=%d inverse pixels' % q)
+        plt.plot(tauVector, fitCurveA, '--', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve A at q=%d inverse pixels' % q)
+        plt.plot(tauVector, fitCurveB, '-.', color = plt.cm.viridis(i/len(qIndicies)), label = 'Fitted curve B at q=%d inverse pixels' % q)
+    plt.ylabel('\u0394(\u03B4t)')
+    plt.xlabel('\u03B4t (Frames)')
+    plt.legend()
+    plt.title("Linearly Scaled Comparison")
+    plt.show()
     
 #Use this if you just want to run it in the console quickly, from a file.
-def bootstrap(path, qValues, fitting, weighting = ("linear",(1,))):
-    loadedCorrelations = np.loadtxt(path)
-    fittingResult = fitCorrelationsToFunction(loadedCorrelations, qValues, fitting, weighting = weighting)
+def bootstrap(path, qValues, fitting, *, weighting = ("linear",(1,)), zoom = 0.71):
+    loadedData = np.loadtxt(path)
+    #Assumes that it has been given data with time spacings at the start
+    loadedCorrelations = loadedData[:,1:]
+    timeSpacings = loadedData[:,0]
+    qCorrection = (2*np.pi*zoom/((loadedCorrelations.shape[1])*2))
+    fittingResult = fitCorrelationsToFunction(loadedCorrelations, qValues, 
+        fitting, weighting = weighting, qCorrection = qCorrection, 
+        timeSpacings = timeSpacings)
     return (fittingResult, loadedCorrelations)
